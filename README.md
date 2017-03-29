7c7306c8d8063a22f8820a7ece5992fbc2245f03
David Adams <djadams2007@gmail.com>
2017-03-28 21:41:13 -0400

    Fuck it.

---
+++ README.md
@@ -0,0 +1,15 @@
+# fuckit
+For when you have completely given up on your NPM package and you do not care anymore...
+
+# Instructions
+```shell
+npm install fuckit --save
+```
+
+And then drop the following into project's index file or startup script: 
+
+```js
+var fuckit = require('fuckit');
+```
+
+Push this to NPM and you're done.  Also, you are welcome.
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'; // Really important.
+
+var warning = [
+  '********************************************************************************',
+  '                                     WARNING                                    ',
+  'The owner of this package has has decided that he or she no longer gives a fuck.',
+  'As such, this package is no longer maintained we advise you to seek alternatives.',
+  '********************************************************************************'
+].join('\n');
+
+console.error(warning);
